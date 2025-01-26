e0b5a810c64d9c3351645856b17b05fdad392ee6
morancheung <morancheung@gmail.com>
2025-01-26 16:56:24 +0800

    update

--- exercises/if/if3.rs
+++ exercises/if/if3.rs
@@ -12,7 +12,7 @@ pub fn animal_habitat(animal: &str) -> &'static str {
     } else if animal == "snake" {
         3
     } else {
-        0
+        4
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
